bceae83ccd4cb33fff66a29806cdb2a15df0dca0
gan214 <824526031@qq.com>
2020-06-09 17:28:41 +0800

    提交到本地仓库

---
+++ git/src/gan/GG.java
@@ -0,0 +1,19 @@
+package gan;
+
+public class GG {
+
+	private int id;
+	private String username;
+	private String password;
+	private int sex;
+	private String love;
+	
+	public GG(int id,String username,String password,int sex,String love) {
+		super();
+		this.id = id;
+		this.username = username;
+		this.password = password;
+		this.sex = sex;
+		this.love = love;
+	}
+}
